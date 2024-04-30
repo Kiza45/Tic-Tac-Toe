b1c7055d97a52ae2108b5daa10eeb2af0b16cfa2
Kiza45 <kieransharp2@gmail.com>
2024-04-30 13:56:52 +0100

    more edits to the game logic

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -45,11 +45,43 @@ class game_logic:
             first_diagonal =[row[i] for i, row in enumerate(rows)]
             second_diagonal = [col[i] for i, col in enumerate(reversed(columns))]
             return rows + columns + [first_diagonal, second_diagonal]
+
+        def is_valid_move(self, move):
+            row, col = move.row, move.col
+            move_was_not_played = self. _current_moved[row][col].label == ""
+            no_winner = not self._has_wineer
+    
+        def process_move():
+            row, col = move.row, move.col
+            self._current_moves[row][col] = move
+            for combo in self._winning_combos:
+                results = set(
+                    self._current_moves[n][m].label
+                    for n,m in combo
+                )
+                is_win = (len(results)==1) and ("" not in results)
+                if is_win:
+                    self._has_winner = True
+                    self.winner_combo = combo
+                    break
+        
+        def has_winner(self):
+            return self._has_winner
+        
+        def it_tied(self):
+            no_winner = not self._has_winner
+            played_moves = (move.label for row in self._current_moves for move in row)
+            return no_winner and all(played_moves)
+        
+        def toggle_players(self):
+            self.current_player = next(self._players)
+        
 class game_board(tk.Tk):
-    def __init__(self):
+    def __init__(self, game):
         super().__init__()
         self.title("Tic Tac Toe")
         self._cells = {}
+        self._game = game
         self.create_game_board()
         self.create_board_grid()
 
@@ -67,10 +99,10 @@ class game_board(tk.Tk):
     def create_board_grid(self):
         grid_frame = tk.Frame(master=self)
         grid_frame.pack()
-        for row in range(3):
+        for row in range(self._game.board_size):
             self.rowconfigure(row, weight=1, minsize=50)
             self.columnconfigure(row, weight=1, minsize=75)
-            for col in range(3):
+            for col in range(self._game.board_size):
                 button = tk.Button(
                     master=grid_frame,
                     text="",
